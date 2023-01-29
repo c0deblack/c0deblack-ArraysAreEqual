66b82a077f40241693625ed8bd78ee9e73596aa9
c0deblack <79178809+c0deblack@users.noreply.github.com>
2023-01-29 00:38:48 +0000

    Completed tests.

--- src/main/java/ArraysAreEqual.java
+++ src/main/java/ArraysAreEqual.java
@@ -15,6 +15,6 @@ public class ArraysAreEqual {
      * @return true if the values of a are equal to the values of b.
      */
     public boolean equal(int[] a, int[] b){
-        return false;
+        return java.util.Arrays.equals(a, b);
     }
 }
